b0a82508a60e74420a35cceba8fa9f1e393f3e54
meera-ramesh19 <meeraramesh19@outlook.com>
2022-06-13 13:26:57 -0400

    Made Links to Home and About Page, added logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,25 @@
 import { React,Component } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+import youtube_logo from '../assets/yt_logo_rgb_dark.png';
 
 class NavBar extends Component {
     render() {
     
     return(
-        <div>NavBar</div>
+        <nav className='navbar'>
+      <div className='logo'>
+        <img src={youtube_logo} width='150px' alt='Youtube logo' />
+      </div>
+      <ul>
+        <li>
+          <Link to='/'>Home </Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
      )
     }
 }

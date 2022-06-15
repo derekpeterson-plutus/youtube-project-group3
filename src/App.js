6c73cc67a1f5b434f414d0243b06bf21a5d8d681
Lakisha Trusty <daprincessanime@gmail.com>
2022-06-14 21:58:52 -0400

    fetchData add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+
+
+fetchData = (inp)=> {
+  fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=21&q=${inp}&type=video&key=${process.env.REACT_APP_API_KEY}`
+  )
+  .then((res) =>{
+    this.setState({
+      return res.json();
+    })
+    .then((data) => {
+      this.setState({
+        video: data.items,
+      });
+    })
+  })
+}
+
   return (
     <div className='App'>
       <title>YouTube App</title>

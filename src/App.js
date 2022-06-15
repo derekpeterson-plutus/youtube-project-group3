a11f33936dc0ce399a37bb888564a99c07fb3ad5
Derek <plutus.derekpeterson@gmail.com>
2022-06-15 10:51:19 -0400

    Add content in About Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar.js';
+import About from './Components/About.js';
+import SearchBar from './Components/SearchBar.js';
+import Page404NotFound from './Components/Page404NotFound.js';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <title>YouTube App</title>
-      <h1>YouTube</h1>
+      <header className='App-header'>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<SearchBar />} />
+          <Route path='/about' element={<About />} />
+          {/* <Route
+            path='*'
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>404 PageThere's nothing here!</p>
+              </main>
+            }
+          /> */}
+          <Route path='*' element={<Page404NotFound />} />
+        </Routes>
+      </header>
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import './About.css';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className='about-page'>
+        <div>
+          <h1>Our Project</h1>
+          <p className='about-paragraph'>
+            This YouTube clone project is created with React Library for better
+            user interface. Users are able to search on Youtube, watch their
+            favorite videos and give their comment.
+          </p>
+        </div>
+        <div className=''>
+          <div className=''>
+            <h1>Meera</h1>
+            <img className='portrait' src='' alt='Meera' />
+            <div>
+              <a
+                href='https://www.linkedin.com/in/meeraramesh'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <img className='logo' src='' alt='' />
+              </a>
+              <a
+                href='https://github.com/meera-ramesh19'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <img className='logo' src='' alt='' />
+              </a>
+            </div>
+            <p className='p-tag'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              mattis, ex nec semper porta, nibh arcu suscipit ligula, et auctor
+              nisi dolor sit amet neque. Praesent porttitor placerat dolor.
+              Donec nec blandit nisl, nec rutrum orci. Aenean ac dignissim
+              felis, euismod pellentesque augue. Proin iaculis odio ut tortor
+              rutrum, a facilisis neque maximus. Nam gravida vulputate ante, in
+              volutpat tortor luctus et. Curabitur suscipit ipsum laoreet quam
+              ultricies, id porttitor erat pharetra. Maecenas sit amet felis ut
+              metus placerat scelerisque. Sed eget mauris dolor. Maecenas
+              bibendum enim lectus. Etiam id purus congue, molestie urna at,
+              vulputate mi. Sed rutrum tincidunt augue, ac accumsan ipsum
+              viverra sed. Sed tristique, mi vel ultricies varius, elit eros
+              rutrum libero, eget feugiat nunc ligula a neque. Cras quis ligula
+              interdum massa elementum vulputate eu at tortor. Pellentesque
+              imperdiet orci in sem laoreet vehicula. Suspendisse potenti.
+            </p>
+          </div>
+          <div className=''>
+            <h1>Lakisha</h1>
+            <img className='portrait' src='' alt='Lakisha' />
+            <div>
+              <a
+                href='linkedin.com/in/lakisha-trusty-101526118'
+                target=''
+                rel='noreferrer noopener'
+              >
+                <img className='logo' src='' alt='' />
+              </a>
+              <a
+                href='https://github.com/daprincessanime'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <img className='logo' src='' alt='' />
+              </a>
+            </div>
+            <p className='p-tag'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              mattis, ex nec semper porta, nibh arcu suscipit ligula, et auctor
+              nisi dolor sit amet neque. Praesent porttitor placerat dolor.
+              Donec nec blandit nisl, nec rutrum orci. Aenean ac dignissim
+              felis, euismod pellentesque augue. Proin iaculis odio ut tortor
+              rutrum, a facilisis neque maximus. Nam gravida vulputate ante, in
+              volutpat tortor luctus et. Curabitur suscipit ipsum laoreet quam
+              ultricies, id porttitor erat pharetra. Maecenas sit amet felis ut
+              metus placerat scelerisque. Sed eget mauris dolor. Maecenas
+              bibendum enim lectus. Etiam id purus congue, molestie urna at,
+              vulputate mi. Sed rutrum tincidunt augue, ac accumsan ipsum
+              viverra sed. Sed tristique, mi vel ultricies varius, elit eros
+              rutrum libero, eget feugiat nunc ligula a neque. Cras quis ligula
+              interdum massa elementum vulputate eu at tortor. Pellentesque
+              imperdiet orci in sem laoreet vehicula. Suspendisse potenti.
+            </p>
+          </div>
+          <div className=''>
+            <h1>Derek</h1>
+            <img className='portrait' src='' alt='' />
+            <div>
+              <a
+                href='https://github.com/derekpeterson-plutus'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <img className='logo' src='' alt='' />
+              </a>
+              <a
+                href='linkedin.com/in/derekpeterson-plutus'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <img className='logo' src='' alt='' />
+              </a>
+            </div>
+            <p className='p-tag'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              mattis, ex nec semper porta, nibh arcu suscipit ligula, et auctor
+              nisi dolor sit amet neque. Praesent porttitor placerat dolor.
+              Donec nec blandit nisl, nec rutrum orci. Aenean ac dignissim
+              felis, euismod pellentesque augue. Proin iaculis odio ut tortor
+              rutrum, a facilisis neque maximus. Nam gravida vulputate ante, in
+              volutpat tortor luctus et. Curabitur suscipit ipsum laoreet quam
+              ultricies, id porttitor erat pharetra. Maecenas sit amet felis ut
+              metus placerat scelerisque. Sed eget mauris dolor. Maecenas
+              bibendum enim lectus. Etiam id purus congue, molestie urna at,
+              vulputate mi. Sed rutrum tincidunt augue, ac accumsan ipsum
+              viverra sed. Sed tristique, mi vel ultricies varius, elit eros
+              rutrum libero, eget feugiat nunc ligula a neque. Cras quis ligula
+              interdum massa elementum vulputate eu at tortor. Pellentesque
+              imperdiet orci in sem laoreet vehicula. Suspendisse potenti.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;

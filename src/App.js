f7394d20dbd5d603ed82ae04b805452da9ef7cbd
Lakisha Trusty <daprincessanime@gmail.com>
2022-06-14 22:49:36 -0400

    fix api key

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+ constructor () { 
+   super();
+   this.state = {
+     video: [],
+   };
+ } 
 
-
-
-fetchData = (inp)=> {
-  fetch(
+ fetchData = (inp)=> {
+   fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=21&q=${inp}&type=video&key=${process.env.REACT_APP_API_KEY}`
   )
   .then((res) =>{
-    this.setState({
       return res.json();
     })
     .then((data) => {
+      console.log(data);
       this.setState({
         video: data.items,
       });
     })
-  })
+   
 }
 
+
+
+
+    render (){
+    
   return (
+
     <div className='App'>
       <title>YouTube App</title>
       <h1>YouTube</h1>
+      {this.fetchData(" ") }
     </div>
   );
 }
-
+}
 export default App;

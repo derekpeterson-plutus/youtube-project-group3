565584879c653fdd03d25a3ba1026aba11454539
meera-ramesh19 <meeraramesh19@outlook.com>
2022-06-21 12:21:50 -0400

    styled navbar videodisplay

--- src/Components/VideoDisplay.js
+++ src/Components/VideoDisplay.js
@@ -2,6 +2,8 @@ import YouTube from 'react-youtube';
 import React, { Component } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import CommentSection from './CommentSection.js';
+import './VideoDisplay.css';
+import { Card, Button, Container, Col } from 'react-bootstrap';
 
 function withParams(Component) {
   return (props) => <Component {...props} params={useParams()} />;
@@ -13,19 +15,52 @@ class VideoDisplay extends Component {
   };
 
   render() {
-    const { id } = this.props.params;
+    const { id, vidtitle } = this.props.params;
+
     return (
-      <div>
-        <section>
+      <>
+        <Container className='p-4'>
+          <Col md='4'>
+            <Card style={{ width: '45rem' }}>
+              <Card.Img />
+              <Card.Body>
+                <Card.Title>{vidtitle}</Card.Title>
+                <Card.Text>
+                  <YouTube
+                    videoId={id}
+                    title={vidtitle}
+                    onReady={this.VideoPlay}
+                  />
+                </Card.Text>
+                <Button variant='danger'>
+                  {' '}
+                  <Link
+                    className='new-search-btn-link'
+                    style={{
+                      textDecoration: 'none',
+                      color: 'white',
+                      backgroundColor: '#ff0000',
+                    }}
+                    to='/'
+                  >
+                    Search New Videos
+                  </Link>
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Container>
+        {/* <section>
           <YouTube videoId={id} onReady={this.VideoPlay} />
           <Link className='new-search-btn-link' to='/'>
             Search New Videos
           </Link>
-        </section>
+        </section>*/}
+
         <section>
           <CommentSection />
         </section>
-      </div>
+      </>
     );
   }
 }
